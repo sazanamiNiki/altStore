883d7808a81c39b7b61dc070727f3067ac95db2c
bunchoniki <bunchoniki@users.noreply.github.com>
2026-02-12 23:51:33 +0900

    [add]: サイトマップ周りの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 const rawBase = process.env.ASTRO_BASE || '/';
 
 export default defineConfig({
   site: 'https://altstore-jp.bunchoniki.com',
   base: rawBase.endsWith('/') ? rawBase : rawBase + '/',
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    mdx(),
-  ],
+  integrations: [tailwind({ applyBaseStyles: false }), mdx(), sitemap()],
   output: 'static',
 });
